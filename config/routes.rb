b14d2936e9a33a000994ee1b98b5ba17593a47e1
Nishant Dalmia <nishant1851@gmail.com>
2014-09-27 11:25:11 +0530

    added alias for routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ require 'resque-queue-priority-server'
   mount Resque::Server.new, :at => "/resque"
 
   root 'search#index'
-  get 'file'  => 'search#file', :as => file
-  get 'files' => 'search#files',  :as => files
-  get 'functions' => 'search#functions', :as => file_functions
+  get 'file'  => 'search#file', :as => :file
+  get 'files' => 'search#files',  :as => :files
+  get 'functions' => 'search#functions', :as => :file_functions
 
   get "*path"   => redirect("404.html")
   post "*path"  => redirect("404.html")
