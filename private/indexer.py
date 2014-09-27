bbf6c885eadb6df90ab7618f44fad1496c6eb51d
Pranav Rao <pnpranavrao@gmail.com>
2014-09-27 15:01:18 +0530

    pygments

--- private/indexer.py
+++ private/indexer.py
@@ -7,6 +7,9 @@ from pprint import pprint
 from subprocess import call
 from git import Repo
 import re
+from pygments import highlight
+from pygments.lexers import get_lexer_for_filename
+from pygments.formatters import HtmlFormatter
 
 INDEX_NAME=sys.argv[1]
 TYPE_NAME=sys.argv[2]
@@ -19,6 +22,13 @@ PREVIEW_SIZE = 50
 FULL_SIZE = 20000
 
 es = Elasticsearch([ELASTICSEARCH_URL])
+
+def pygmentize(filename, filestring):
+	lexer = get_lexer_for_filename(filename)
+	formatter = HtmlFormatter(cssclass="source", linespans="line", nowrap=True)
+	f = open("test.html", 'w')
+	result = highlight(filestring, lexer, formatter)
+	return result
 
 def get_paths(path, repo_url):
 	for root, dirs, files in os.walk(path, topdown=False):
@@ -40,24 +50,15 @@ def get_paths(path, repo_url):
 				f.seek(0)
 				for line in f:
 					if line_number <= PREVIEW_SIZE:
-						preview += line + "<br>"
-					content += line + "<br>"
+						preview += line
+					content += line
 					line_number += 1
 					function_name = find_function(line)
 					if function_name:
 						body2 = {"path": path_name, "function_name": function_name, "line_number":line_number, "repo_url" : repo_url}
 						es.index(index=INDEX_NAME, doc_type=TYPE_NAME_FUNC, body=body2)
-			body = {"path":path_name, "name":name, "body":content, "body_preview": preview, "repo_url" : repo_url }
+			body = {"path":path_name, "name":name, "body":pygmentize(name,content), "body_preview": pygmentize(name, preview), "repo_url" : repo_url }
 			es.index(index=INDEX_NAME, doc_type=TYPE_NAME, body=body)
-
-# def read_in_chunks(file_object, chunk_size=1024):
-#     """Lazy function (generator) to read a file piece by piece.
-#     Default chunk size: 1k."""
-#     while True:
-#         data = file_object.read(chunk_size)
-#         if not data:
-#             break
-#         yield data
 
 def find_function(line):
 	regex = re.compile("\sdef\s(.+)\s")
