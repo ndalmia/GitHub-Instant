abdbab4df444d366421660b00f4ff9bd4ce3fb38
Nishant Dalmia <nishant1851@gmail.com>
2014-09-27 12:31:37 +0530

    faye url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def broadcast(channel)
     message = {:channel => channel, :data => "PROCESSED"}
-    uri = URI.parse(Figaro.env["faye_url"])
+    uri = URI.parse("http://10.1.1.225:9292")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 end
