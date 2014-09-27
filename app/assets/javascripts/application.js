ad8b789bb1f5f133e0fe23223b2ad55a5c8f0197
Nishant Dalmia <nishant1851@gmail.com>
2014-09-27 09:18:58 +0530

    updated faye js code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,22 +15,34 @@
 //= require turbolinks
 //= require_tree .
 
-$(function() {
-  var faye = new Faye.Client(Figaro.env['faye_url']);
-});
-
-var subscribe_to_channel = function(channel) {
-  faye.subscribe(channel, function (data) {
-    alert(data);
-  	if(data == "PROCESSED") {
-  		location.reload;
-  	}
-  });
-}
-
 $(document).ready(function () {
-	if(location.pathname != "/") {
-		var channel = location.pathname;
-		subscribe_to_channel(channel)
-	}
-}
+  function getQueryParams () {
+    function identity (e) { return e; }
+    function toKeyValue (params, param) {
+      var keyValue = param.split('=');
+      var key = keyValue[0], value = keyValue[1];
+
+      params[key] = params[key]?[value].concat(params[key]):value;
+      return params;
+    }
+    return decodeURIComponent(window.location.search).
+      replace(/^\?/, '').split('&').
+      filter(identity).
+      reduce(toKeyValue, {});
+  }
+
+  var faye = new Faye.Client("http://10.1.1.225:9292/faye");
+  var subscribe_to_channel = function(channel) {
+    faye.subscribe(channel, function (data) {
+      if(data == "PROCESSED") {
+        location.reload;
+      }
+    });
+  }
+  
+  if(location.pathname == "/") {
+   params = getQueryParams();
+    var channel = "/"+params.repo;
+    subscribe_to_channel(channel);
+  }
+});
